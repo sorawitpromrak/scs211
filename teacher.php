2410404ba7071329d484f1c8295ffa2662c0bca4
sorawitpromrak <118663710+sorawitpromrak@users.noreply.github.com>
2023-02-26 18:45:04 +0700

    Week12

---
+++ teacher.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include("layouts/css.php") ?>
+</head>
+
+<body>
+    <!---this is navigation / header -->
+    <?php include("layouts/header.php") ?>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Teacher</h1>
+        <p class="lead d-none">Quickly build an effective pricing table for your potential customers with this Bootstrap
+            example. It’s built with default Bootstrap components and utilities with little customization.</p>
+    </div>
+
+    <!-- content -->
+    <div class="container">
+        <div class="text-center">
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>ชื่อสกุล</th>
+                        <th>รายละเอียด</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>อาจารย์สุนี ปัญจะเทวคุปก์ </td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>อาจารย์วิวัฒน์ ชินนาทศิริกุล์</td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>3 </td>
+                        <td>อาจารย์ดาวรถา วีระพันธ์</td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>อาจารย์ณัฎฐิรา ศุขไพบูลย์์</td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>5 </td>
+                        <td>อาจารย์ประณมกร อัมพรพรรดิ์</td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>6 </td>
+                        <td>อ่าจารย์วิศรุต ขวัญคุ้ม</td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>7 </td>
+                        <td>อาจารย์ณัฐรดี อนุพงค์ </td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+                    <tr>
+                        <td>8 </td>
+                        <td>อาจารย์ชวลิต โควีระวงศ์</td>
+                        <td><a href="#">คลิก</a></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+
+
+        <!-- footer -->
+        <?php include("layouts/footer.php") ?>
+
+    </div>
+</body>
+
+</html>
