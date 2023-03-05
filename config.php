2c2b53f9dac188e1b7edac01003d01b10b453a3b
sorawitpromrak <118663710+sorawitpromrak@users.noreply.github.com>
2023-03-05 16:46:43 +0700

    week08 (New)

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '123456789');
+define('DB_PASSWORD', '12345678');
 define('DB_NAME', 'scs211');
  
 /* Attempt to connect to MySQL database */
